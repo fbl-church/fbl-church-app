70287824092689e41ec8a678513f489a77957f8d
Samuel Butler <sambutler1017@icloud.com>
2023-04-22 12:44:47 -0400

    no message

--- projects/insite-kit/src/component/grid/grid-checklist-column/grid-checklist-column.component.ts
+++ projects/insite-kit/src/component/grid/grid-checklist-column/grid-checklist-column.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ik-grid-checklist-column',
-  templateUrl: './grid-checklist-column.component.html',
+  template: '',
 })
 export class GridChecklistColumnComponent {}
