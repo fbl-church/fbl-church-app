15b9386206cf967a7ba006837c38b75eb0555537
Samuel Butler <sambutler1017@icloud.com>
2023-06-17 19:05:55 -0400

    REMOVE

--- src/app/pages/login/login-overview/login-overview.component.ts
+++ src/app/pages/login/login-overview/login-overview.component.ts
@@ -41,7 +41,7 @@ export class LoginOverviewComponent implements OnInit {
       .authenticate(this.form.value.username, this.form.value.password)
       .subscribe({
         next: (res) => {
-          this.router.navigate(['/home']);
+          this.router.navigate(['/users']);
           this.loading = false;
         },
         error: (err) => {
