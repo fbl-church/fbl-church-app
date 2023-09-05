0f6fdc80c605bbf7403891336f04f87a93f6295c
Samuel Butler <sambutler1017@icloud.com>
2023-09-05 18:48:05 -0400

    no message

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BaseInitModule } from 'src/app/common/base-init.module';
-import { CreateAccountComponent } from './create-account/create-accountcomponent';
 import { LoginOverviewComponent } from './login-overview/login-overview.component';
 import { LoginComponent } from './login.component';
 
 @NgModule({
   imports: [BaseInitModule],
-  declarations: [
-    LoginComponent,
-    LoginOverviewComponent,
-    CreateAccountComponent,
-  ],
+  declarations: [LoginComponent, LoginOverviewComponent],
 })
 export class LoginModule {}
