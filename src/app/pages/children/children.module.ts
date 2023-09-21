57595d73ac031bb9d6e703bd528420557086d6cc
Samuel Butler <sambutler1017@icloud.com>
2023-09-21 19:17:38 -0400

    no message

--- src/app/pages/children/children.module.ts
+++ src/app/pages/children/children.module.ts
@@ -3,7 +3,7 @@ import { BaseInitModule } from 'src/app/common/base-init.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ChildDetailComponent } from './child-detail/child-detail.component';
 import { DeleteChildModalComponent } from './child-detail/modals/delete-child-modal/delete-child-modal.component';
-import { EditChildGuardiansComponent } from './child-detail/pages/edit-child-guardians/edit-child-gurdians.component';
+import { EditChildGuardiansComponent } from './child-detail/pages/edit-child-guardians/edit-child-guardians.component';
 import { EditChildComponent } from './child-detail/pages/edit-child/edit-child.component';
 import { ChildrenComponent } from './children.component';
 import { CreateChildComponent } from './create-child/create-child.component';
