86b6b334f3549ce0b910c6dc7d84d979f83ae139
Samuel Butler <sambutler1017@icloud.com>
2024-01-28 21:36:09 -0500

    no message

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ app.use(compression());
 // Serve static files from the dist directory
 app.use(express.static(__dirname + '/dist/fbl-church-app'));
 
-// Start the app by listening on the default Heroku port
-app.listen(port);
-
 // Return index.html for all GET requests for PathLocationStrategy
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/fbl-church-app/index.html'));
 });
-console.log(`Server listening on ${port}`);
+
+console.log('Application Succesfully Started!');
+app.listen(process.env.PORT || 8080);
